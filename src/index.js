784601775fe88ba9a165ae1aa5d2973ecf39959b
Lovchinskaya <33342414+Lovchinskaya@users.noreply.github.com>
2023-05-31 19:51:06 +0300

    HW 10
    2 commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-const BASE_URL = "https://api.thecatapi.com/v1/breeds";
-const API = "live_8LD487u3PW0ACw4mo3clIsgLpSj0QCO01hF4spptpbumXSHheMAfCVCsK7GblUyB";
 
-function fetchEvents(keyword) {
-    const params = new URLSearchParams({
-        apikey: API,
-        value: breed_ids,
-        category_ids
-        });
-    return fetch(`${BASE_URL}?${params}`)
-      .then((response) => {
-        // console.log(response);
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .catch((error) => console.log(error));
-  }
+import Notiflix from 'notiflix';
+import { fetchCats } from './cat-api';
