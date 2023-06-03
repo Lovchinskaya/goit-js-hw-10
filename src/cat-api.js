a4750554e1b9ce97200bb25835f55d84391231c2
Lovchinskaya <33342414+Lovchinskaya@users.noreply.github.com>
2023-06-03 09:09:06 +0300

    hw 10
    3.06 1 commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,9 +1,16 @@
-export function fetchCountries(value) {
-    return fetch(`https://api.thecatapi.com/v1/breeds${value}?breed_ids=beng,abys`)
-        .then(response => {
+const API = "live_8LD487u3PW0ACw4mo3clIsgLpSj0QCO01hF4spptpbumXSHheMAfCVCsK7GblUyB";
+const UrlGeneral = "https://api.thecatapi.com/v1/breeds"
+export function fetchBreeds(keyword) {
+    return fetch(`${UrlGeneral}?api_key=${API}`)
+        .then((response) => {
         if (!response.ok) {
             throw new Error(response.status);
         }
         return response.json();    
-    });
+    })
+    .catch((error) => console.log(error));
 }
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 
+import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-import { fetchCats } from './cat-api';
+import { fetchBreeds } from './cat-api';
+
+const selectDropDown = document.querySelector(".breed-select");
+
+function getEvents(query){
+   fetchBreeds(query).then(data => {
+
+const events = data;
+//renderEvents(events)
+//console.log(events);
+   })
+}
+getEvents('cat')
+
+//console.log()
+
+//console.log(fetchBreeds('cat'))
+
+/*function renderBreeds(breeds){
+    const markup = breeds.map((breed) => {
+return `<li><h2>${breed.name}</h2><img src="${breed.reference_image_id}" alt="" width="200px"></img></li>`
+    }).join('');
+    ulList.insertAdjacentHTML('afterend', markup)
+    console.log(markup)
+}
+
+  <option value="${breed.name}">${breed.name}</option>*/
